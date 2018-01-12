733bc5491966fdd6e7cf4ffb1f0f341508d133b1
alex <student@alex-buzali.dalton.org>
2018-01-12 15:18:43 -0500

    kj

--- src/BungeeJump.java
+++ src/BungeeJump.java
@@ -1,8 +1,14 @@
 import org.opensourcephysics.controls.AbstractSimulation;
+import org.opensourcephysics.display.Trail;
+import org.opensourcephysics.frames.DisplayFrame;
 
 public class BungeeJump extends AbstractSimulation{
 
 	protected void doStep() {
+		
+		DisplayFrame ballFrame = new DisplayFrame("x", "y", "Ball Motion");
+		Trail cord = new Trail();
+		
 		
 	}
 
