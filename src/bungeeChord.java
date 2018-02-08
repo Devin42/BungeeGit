bc4259f3a591d7be5436273b0e139c8cd38592a9
student <student@devin-milberg.dalton.org>
2018-02-08 10:45:08 -0500

    onstructor

--- src/bungeeChord.java
+++ src/bungeeChord.java
@@ -6,4 +6,13 @@ public class bungeeChord {
 	double segmentNumber; //number of segments
 	double k; //spring constant
 	
+	public bungeeChord(double cordLength, double ropeMass, double personMass, double numberOfSegments) {
+		this.cordLength = cordLength;
+		this.ropeMass = ropeMass;
+		this.personMass = personMass;
+		this.segmentNumber = numberOfSegments;
+	}
+	
+	
+	
 }
