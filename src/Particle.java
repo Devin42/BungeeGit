fcdb54bd200c83f20b0f868e3819bc57d7c55025
alex <student@alex-buzali.dalton.org>
2018-02-21 16:00:37 -0500

    final

--- src/Particle.java
+++ src/Particle.java
@@ -1,17 +1,23 @@
 public class Particle {
 
+	//Particle's position, only need one because it's one dimensional motion
 	double position;
 	
+	//Particle's velocity
 	double velocity;
-	double velocityLast;
 	
+	//Particle's acceleration
 	double acceleration;
 	
+	//How far down the cord the particle
 	int orderPosition;
 	
+	//How far the particle is stretched from its original position
 	double deltaX;
 	
+	//The force from the spring above the particle
 	double springForceUp;
 	
+	//The force from the spring below the particle
 	double springForceDown;
 }
