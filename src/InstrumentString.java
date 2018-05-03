1cc0b971776d4908b93bd77fb6ecf4b20acd4320
student <student@devin-milberg.dalton.org>
2018-05-03 10:33:13 -0400

    new thing

--- src/InstrumentString.java
+++ src/InstrumentString.java
@@ -1,8 +1,44 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+import org.opensourcephysics.controls.AbstractSimulation;
+import org.opensourcephysics.controls.SimulationControl;
+import org.opensourcephysics.display.Circle;
+import org.opensourcephysics.display.DrawableShape;
+import org.opensourcephysics.display.Trail;
+import org.opensourcephysics.frames.DisplayFrame;
 
 
-public class Hair {
+public class InstrumentString extends AbstractSimulation{
 
-	//1m string, 100 mass, 10g total  
-	
-	
+	//1m string, 100 mass, 10g total
+	//Cord variables
+	double cordLength;
+	double cordMass;
+	double springConstant;
+	double numMasses;
+	double timeStep;
+
+
+
+	protected void doStep() {
+		// TODO Auto-generated method stub
+
+	}
+	public void reset() {
+
+		control.setValue("Time Step", .01);
+
+
+	}
+
+	public void initialize() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
 }
+
