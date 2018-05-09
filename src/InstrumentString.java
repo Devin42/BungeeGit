d52758384e86871fe8e807490cfb5756f176c264
alex <student@alex-buzali.dalton.org>
2018-05-09 13:06:04 -0400

    jknkj

--- src/InstrumentString.java
+++ src/InstrumentString.java
@@ -18,29 +18,48 @@ public class InstrumentString extends AbstractSimulation{
 	//Cord variables
 	double cordLength;
 	double cordMass;
-	double totalspringConstant;
+	double totalSpringConstant;
 	double numMasses;
 	double individualSpringConstant;
 	double timeStep;
+	double totalTime;
 	
 	//ArrayList that holds all the particles, as well as an ArrayList for the corresponding circles that show up on the frame
 	ArrayList <Particle> particleArray = new ArrayList <Particle>();
 	ArrayList <Circle> circleArray = new ArrayList <Circle>();
 
 	protected void doStep() {
-		// TODO Auto-generated method stub
-
+	
+		//Makes program fun faster
+		for (int z = 0; z < 100; z++) {
+		
+			particleArray.get(0).yPosition = .1*Math.sin(totalTime);
+			circleArray.get(0).setY(.1*Math.sin(totalTime));
+			
+			for (int i = 1; i < numMasses - 1; i++) {
+				updatePosition(particleArray.get(i));
+				circleArray.get(i).setXY(particleArray.get(i).xPosition, particleArray.get(i).yPosition);
+				
+			}
+			
+			totalTime += timeStep;
+			System.out.println();
+		}
 	}
+	
 	public void reset() {
-		control.setValue("Time Step", .01);
-		control.setValue("Spring Constant", 17.6);
+		control.setValue("Time Step", .0001);
+		control.setValue("Spring Constant", 2000);
 		control.setValue("Cord Mass", .01);
 		control.setValue("Cord Length", 1);
 		control.setValue("Number of Masses", 50);
 	}
 
 	public void initialize() {
-
+		
+		//Makes doStep run faster
+		this.setDelayTime(0);
+		
 		//Frame settings
 		frame.setPreferredMinMax(-.1, 1.5, -.15, .15);
 		frame.setSize(600, 600);
@@ -48,11 +67,11 @@ public class InstrumentString extends AbstractSimulation{
 		
 		//Prefer to use global variables instead of having to write control.getDouble every time
 		timeStep = control.getDouble("Time Step");
-		totalspringConstant = control.getDouble("Spring Constant");
-		individualSpringConstant = totalspringConstant*numMasses;
+		totalSpringConstant = control.getDouble("Spring Constant");
 		cordMass = control.getDouble("Cord Mass");
 		cordLength = control.getDouble("Cord Length");
 		numMasses= control.getDouble("Number of Masses");
+		individualSpringConstant = totalSpringConstant*numMasses;
 
 		//Adds the circles representing each particle to the frame
 		for (int i = 0; i < numMasses; i++) {
@@ -66,6 +85,7 @@ public class InstrumentString extends AbstractSimulation{
 			}
 			
 			particle.orderPosition = i;
+			particle.mass = cordMass / numMasses;
 			
 			circle.pixRadius = 5;
 			
@@ -80,7 +100,7 @@ public class InstrumentString extends AbstractSimulation{
 		
 	}
 
-	public void netForce (Particle particle) {
+	/*public void netForce (Particle particle) {
 		
 		//Force from the particle before
 		Particle previousParticle = particleArray.get(particle.orderPosition - 1);
@@ -94,6 +114,56 @@ public class InstrumentString extends AbstractSimulation{
 		
 		
 		
+		
+	}*/
+	
+	//Returns the force in the x direction on a particle
+	public double xForce (Particle particle) {
+		
+		//Particles before and after the current one on the string
+		Particle previousParticle = particleArray.get(particle.orderPosition - 1);
+		Particle nextParticle = particleArray.get(particle.orderPosition + 1);
+		
+		//Forces in the x direction from both particles (kx * cos)
+		double previousForce = individualSpringConstant * distanceFrom(previousParticle, particle) * 
+		(previousParticle.xPosition - particle.xPosition)/distanceFrom(previousParticle,particle);
+		
+		double nextForce = individualSpringConstant * distanceFrom(nextParticle, particle) * 
+		(nextParticle.xPosition - particle.xPosition)/distanceFrom(nextParticle,particle);
+		
+		return nextForce + previousForce;
+		
+	}
+	
+	//Returns the force in the y direction on a particle
+	public double yForce (Particle particle) {
+		
+		//Particles before and after the current one on the string
+		Particle previousParticle = particleArray.get(particle.orderPosition - 1);
+		Particle nextParticle = particleArray.get(particle.orderPosition + 1);
+		
+		//Forces in the x direction from both particles (kx * cos)
+		double previousForce = individualSpringConstant * distanceFrom(previousParticle, particle) * 
+		(previousParticle.yPosition - particle.yPosition)/distanceFrom(previousParticle,particle);
+		
+		double nextForce = individualSpringConstant * distanceFrom(nextParticle, particle) * 
+		(nextParticle.yPosition - particle.yPosition)/distanceFrom(nextParticle,particle);
+		
+		return nextForce + previousForce;
+		
+	}
+	
+	//Updates the position of the particle
+	public void updatePosition (Particle particle) {
+		
+		particle.xAcceleration = xForce(particle)/particle.mass;
+		particle.yAcceleration = yForce(particle)/particle.mass;
+		
+		particle.xVelocity += particle.xAcceleration * timeStep;
+		particle.yVelocity += particle.yAcceleration * timeStep;
+		
+		particle.xPosition += particle.xVelocity * timeStep;
+		particle.yPosition += particle.yVelocity * timeStep;
 		
 	}
 	
@@ -131,12 +201,11 @@ public class InstrumentString extends AbstractSimulation{
 		
 		return angleinfo;
 	}
-	public double distanceFrom(Particle P1, Particle P2) {
-		double distanceFrom = Math.sqrt(Math.pow((P1.xPosition - P2.xPosition), 2) + Math.pow((P1.yPosition - P2.yPosition), 2));
-		
-		return distanceFrom;
-	}
 	
+	//Returns the distance between two particles
+	public double distanceFrom(Particle P1, Particle P2) {
+		return Math.sqrt(Math.pow((P1.xPosition - P2.xPosition), 2) + Math.pow((P1.yPosition - P2.yPosition), 2));
+	}
 	
 	public static void main(String[] args) {
 		SimulationControl.createApp(new InstrumentString());
