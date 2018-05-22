367a09818a46e06fa6ff106149629c4c7b4921da
alex <student@alex-buzali.dalton.org>
2018-05-21 22:59:45 -0400

    khg

--- src/Trampoline.java
+++ src/Trampoline.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
 import org.opensourcephysics.controls.AbstractSimulation;
@@ -19,22 +20,45 @@ public class Trampoline extends AbstractSimulation{
 	double timeStep;
 	int springNumber;
 	int particlesPerSpring;
+	double springMass;
 	double trampolineRadius;
 	double totalSpringConstant;
 	double individualSpringConstant;
 	double restLength;
+	double totalTime;
+	
+	Particle centerParticle = new Particle();
+	Element centerSphere = new ElementEllipsoid();
 	
 	protected void doStep() {
 		
+		for (int z = 0; z < 100; z++) {
+		
+			centerParticle.zPosition = .1 * Math.sin(totalTime);
+			centerSphere.setZ(centerParticle.zPosition);
+			
+			for (int i = 0; i < springArray.size(); i++) {
+				
+				for (int j = 1; j < springArray.get(i).size() - 1; j++) {
+					updatePosition(springArray.get(i).get(j));
+					springSphereArray.get(i).get(j).setXYZ(springArray.get(i).get(j).xPosition, springArray.get(i).get(j).yPosition, 
+					springArray.get(i).get(j).zPosition);
+				}
+				
+			}
+			
+			totalTime += timeStep;
+		}
 	}
 	
 	public void reset() {
 		
-		control.setValue("Time Step", 1E-6);
+		control.setValue("Time Step", 1E-3);
 		control.setValue("Number of Springs", 8);
 		control.setValue("Particles Per Spring", 10);
 		control.setValue("Trampoline Radius", 1);
 		control.setValue("Spring Constant", 100);
+		control.setValue("Spring Mass", 1);
 		control.setValue("Rest Length", 0);
 		
 	}
@@ -49,12 +73,27 @@ public class Trampoline extends AbstractSimulation{
 		trampolineRadius = control.getDouble("Trampoline Radius");
 		totalSpringConstant = control.getDouble("Spring Constant");
 		individualSpringConstant = totalSpringConstant*springNumber;
+		springMass = control.getDouble("Spring Mass");
 		restLength = control.getDouble("Rest Length");
+		
+		centerParticle.xPosition = 0;
+		centerParticle.yPosition = 0;
+		centerParticle.zPosition = 0;
+		centerParticle.mass = 10;
+		
+		centerSphere.setXYZ(0, 0, 0);
+		centerSphere.setSizeXYZ(.1, .1, .1);
+		centerSphere.getStyle().setFillColor(Color.RED);
+		
+		
+		frame.addElement(centerSphere);
 		
 		for (int i = 0; i < springNumber; i++) {
 			
 			ArrayList<Particle> spring = new ArrayList<Particle>();
 			ArrayList<Element> springSpheres = new ArrayList<Element>();
+			
+			spring.add(centerParticle);
 			
 			for (int j = 0; j < particlesPerSpring; j++) {
 				
@@ -72,7 +111,8 @@ public class Trampoline extends AbstractSimulation{
 				sphere.setSizeXYZ(.1, .1, .1);
 				
 				particle.springPosition = i;
-				particle.orderPosition = j;
+				particle.orderPosition = j + 1;
+				particle.mass = springMass/particlesPerSpring;
 				
 				spring.add(particle);
 				springSpheres.add(sphere);
@@ -83,7 +123,6 @@ public class Trampoline extends AbstractSimulation{
 			
 			springArray.add(spring);
 			springSphereArray.add(springSpheres);
-			
 		}
 		
 	}
@@ -101,6 +140,7 @@ public class Trampoline extends AbstractSimulation{
 		double nextForce = individualSpringConstant * (distanceFrom(nextParticle, particle) - restLength) * 
 				(nextParticle.xPosition - particle.xPosition)/distanceFrom(nextParticle,particle);
 		
+		System.out.println(nextForce + previousForce);
 		return nextForce + previousForce;
 	}
 	
@@ -153,13 +193,16 @@ public class Trampoline extends AbstractSimulation{
 
 		}
 	
-	
+	//Calculates the distance between two particles
 	public double distanceFrom (Particle P1, Particle P2) {
-		return Math.sqrt(Math.pow((P1.xPosition - P2.xPosition), 2) + Math.pow((P1.yPosition - P2.yPosition), 2) + Math.pow((P1.zPosition - P2.zPosition), 2));
+		return Math.sqrt(Math.pow((P1.xPosition - P2.xPosition), 2) + Math.pow((P1.yPosition - P2.yPosition), 2) 
+		+ Math.pow((P1.zPosition - P2.zPosition), 2));
 	}
 	
+	//Calculates the distance between two particles using the lastPostion
 	public double distanceFromLast (Particle P1, Particle P2) {
-		return Math.sqrt(Math.pow((P1.xPositionLast - P2.xPosition), 2) + Math.pow((P1.yPositionLast - P2.yPosition), 2) + Math.pow((P1.zPositionLast - P2.zPosition), 2));
+		return Math.sqrt(Math.pow((P1.xPositionLast - P2.xPosition), 2) + Math.pow((P1.yPositionLast - P2.yPosition), 2) 
+		+ Math.pow((P1.zPositionLast - P2.zPosition), 2));
 	}
 	
 	public static void main(String[] args) {
